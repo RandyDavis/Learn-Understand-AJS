05ce2e9ccaa639beccd2ce5976eb6323a5e44734
Randy Davis <randy_davis@outlook.com>
2015-04-06 21:10:34 -0500

    complete lesson 13 - Functions and Strings

--- Section3-Services_and_Dependency_Injection/app.js
+++ Section3-Services_and_Dependency_Injection/app.js
@@ -1,11 +1,13 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainController', function($scope) {
-  $scope.name = "Randy";
-  $scope.occupation = "Web Developer";
-  $scope.getName = function() {
-    return ("Randy");
-  };
-  $scope.getName();
-  console.log($scope);
+  
 });
+
+var searchPeople = function(firstName, lastName, height, age, occupation) {
+  return 'Jane Doe';
+};
+
+var searchPeopleString = searchPeople.toString();
+
+console.log(searchPeopleString);
