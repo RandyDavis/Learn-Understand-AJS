7f7927d5fb0875c62bea77d5df9e570a1e04e74d
Randy Davis <randy_davis@outlook.com>
2015-04-06 21:15:24 -0500

    complete lesson 13 - how angular does dependency injection

--- Section3-Services_and_Dependency_Injection/app.js
+++ Section3-Services_and_Dependency_Injection/app.js
@@ -1,13 +1,11 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainController', function($scope) {
-  
+  console.log($scope);
 });
 
 var searchPeople = function(firstName, lastName, height, age, occupation) {
   return 'Jane Doe';
 };
 
-var searchPeopleString = searchPeople.toString();
-
-console.log(searchPeopleString);
+console.log(angular.injector().annotate(searchPeople));
