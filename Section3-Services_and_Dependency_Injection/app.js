cea140b4775196e2bef614460a9cb0320d80e1ea
Randy Davis <randy_davis@outlook.com>
2015-04-06 23:31:00 -0500

    finish lesson 15 - arrays and functions

--- Section3-Services_and_Dependency_Injection/app.js
+++ Section3-Services_and_Dependency_Injection/app.js
@@ -1,26 +1,10 @@
-var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+var things = [
+    1
+  , '2'
+  , function() {
+    console.log("Hello!");
+  }
+];
 
-myApp.controller('MainController', function($log, $scope, $filter, $resource) {
-  // THE $log SERVICE
-
-  // console.log($scope);
-  // console.log($log);
-  // $log.log("Hello");
-  // $log.info("This is some info");
-  // $log.warn("Warning!");
-  // $log.debug("Some debug information while writing my code.");
-  // $log.error("This is an error.");
-
-
-  // THE $filter SERVICE 
-  // $scope.name = "John";
-  // $scope.formattedName = $filter('uppercase')($scope.name);
-
-  // $log.info($scope.name);
-  // $log.info($scope.formattedName);
-
-
-  
-  console.log($resource);
-
-});
+things[2]();
+console.log(things);
