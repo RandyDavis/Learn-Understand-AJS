b16eb64ceb134336921f4101aca56fa9d8b57b85
Randy Davis <randy_davis@outlook.com>
2015-04-06 23:20:50 -0500

    complete lesson 14 - getting other services

--- Section3-Services_and_Dependency_Injection/app.js
+++ Section3-Services_and_Dependency_Injection/app.js
@@ -1,11 +1,26 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('MainController', function($scope) {
-  console.log($scope);
+myApp.controller('MainController', function($log, $scope, $filter, $resource) {
+  // THE $log SERVICE
+
+  // console.log($scope);
+  // console.log($log);
+  // $log.log("Hello");
+  // $log.info("This is some info");
+  // $log.warn("Warning!");
+  // $log.debug("Some debug information while writing my code.");
+  // $log.error("This is an error.");
+
+
+  // THE $filter SERVICE 
+  // $scope.name = "John";
+  // $scope.formattedName = $filter('uppercase')($scope.name);
+
+  // $log.info($scope.name);
+  // $log.info($scope.formattedName);
+
+
+  
+  console.log($resource);
+
 });
-
-var searchPeople = function(firstName, lastName, height, age, occupation) {
-  return 'Jane Doe';
-};
-
-console.log(angular.injector().annotate(searchPeople));
