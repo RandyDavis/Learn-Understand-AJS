381c038dd48415a8459e6b3262ff7a41d9c56bc3
Randy Davis <randy_davis@outlook.com>
2015-04-08 22:41:55 -0500

    complete scope and interpolation lesson

--- Section4-Data_Binding_and_Directives/app.js
+++ Section4-Data_Binding_and_Directives/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MainController', ['$scope', function ($scope) {
+myApp.controller('MainController', ['$scope', '$timeout', function ($scope, $timeout) {
   
+  $scope.name = "Randy";
+
+  $timeout(function() {
+    $scope.name = 'Everybody';
+  }, 3000);
 }]);
