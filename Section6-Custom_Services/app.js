12abc4b992132d8b62902e2ba92f4a0c00196264
Randy Davis <randy_davis@outlook.com>
2015-04-15 00:07:23 -0500

    update index and app.js files and began work on creating a service lesson;

---
+++ Section6-Custom_Services/app.js
@@ -0,0 +1,33 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'MainController'
+    })
+
+    .when('/second', {
+      templateUrl: 'pages/second.html',
+      controller: 'SecondController'
+    });
+});
+
+myApp.service('nameService', function() {
+  this.name = "John Doe";
+  var self = this;
+  this.namelength = function() {
+    return self.name.length;
+  };
+});
+
+myApp.controller('MainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+  $scope.name = 'Main';
+
+  $log.log(nameService.name);
+  $log.log(nameService.namelength());
+}]);
+
+myApp.controller('SecondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
+  $scope.num = $routeParams.num || 1;
+}]);
