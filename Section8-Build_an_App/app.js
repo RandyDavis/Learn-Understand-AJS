78f94896d5b9b6c67650e53f192f82bb2324aae7
Randy Davis <randy_davis@outlook.com>
2015-04-18 17:52:14 -0500

    complete custom service for weather app lesson

--- Section8-Build_an_App/app.js
+++ Section8-Build_an_App/app.js
@@ -17,12 +17,22 @@ weatherApp.config (function($routeProvider) {
     });
 });
 
-// CONTROLLERS
-
-weatherApp.controller('HomeCtrl', ['$scope', function ($scope) {
-  
+// SERVICES
+weatherApp.service('cityService', [function () {
+  this.city = "Austin, Tx";
 }]);
 
-weatherApp.controller('ForecastCtrl', ['$scope', function ($scope) {
+// CONTROLLERS
 
+weatherApp.controller('HomeCtrl', ['$scope', 'cityService', function ($scope, cityService) {
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+
+}]);
+
+weatherApp.controller('ForecastCtrl', ['$scope', 'cityService', function ($scope, cityService) {
+  $scope.city = cityService.city;
 }]);
