b387e0a8344aea192fba05efe6d03529c6df6ac8
Randy Davis <randy_davis@outlook.com>
2015-04-19 00:42:10 -0500

    completed designing services in large angularjs apps lesson

--- Section9-Bonus_Lectures/services.js
+++ Section9-Bonus_Lectures/services.js
@@ -2,3 +2,13 @@
 weatherApp.service('cityService', [function () {
   this.city = "Austin, Tx";
 }]);
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+  this.GetWeather = function(city, days) {
+    var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+      callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }
+    });
+
+    return weatherAPI.get({ q: city, cnt: days });
+  };
+}]);
